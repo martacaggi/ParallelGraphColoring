57ea301537d1fecd9ced0e4706c36d69cd770c70
Francesco Rabezzano <francescorabezzano@gmail.com>
2021-12-15 12:04:47 +0100

    chore: create project folder

---
+++ cmake-build-debug/CMakeFiles/ParallelGraphColoring.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ParallelGraphColoring.dir/main.cpp.obj"
+  "ParallelGraphColoring.exe"
+  "ParallelGraphColoring.exe.manifest"
+  "ParallelGraphColoring.pdb"
+  "libParallelGraphColoring.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ParallelGraphColoring.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
